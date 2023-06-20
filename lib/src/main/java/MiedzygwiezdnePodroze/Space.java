11d8945548055598640a89f866720b68292cd721
esth8 <estera.leszczynska.85@gmail.com>
2023-06-20 23:25:18 +0200

    added colors in output

--- lib/src/main/java/MiedzygwiezdnePodroze/Space.java
+++ lib/src/main/java/MiedzygwiezdnePodroze/Space.java
@@ -11,15 +11,35 @@ public class Space {
 		return map;
 	}
 	
-	// wypisanie mapy
+	// wypisanie mapy w kolorach
 	public void printSpace (Square[][] map, int n) {
 		for (int i=0; i<n; i++) {
 			for (int j=0; j<n; j++) {
-				if (map[j][i].type=='\0') {
+				switch(map[j][i].type) {
+				case '\0': {
 					System.out.print("_");
+					break;
 				}
-				else { 
-					System.out.print(map[j][i].type);
+				case 'T': {
+					System.out.print("\033[0;46m"+"T"+"\033[0m");
+					break;
+				}
+				case 'L': {
+					System.out.print("\033[0;42m"+"L"+"\033[0m");
+					break;
+				}
+				case 'P': {
+					System.out.print("\033[0;45m"+"P"+"\033[0m");
+					break;
+				}
+				case 'H': {
+					System.out.print("\033[0;41m"+"H"+"\033[0m");
+					break;
+				}
+				case 'A': {
+					System.out.print("\033[0;43m"+"A"+"\033[0m");
+					break;
+				}
 				}
 				System.out.print(" ");
 			}
